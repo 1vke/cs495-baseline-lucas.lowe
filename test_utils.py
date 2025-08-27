a9f162d8b5c860f217955730a3e2998c6e20226b
Lucas Lowe <lowel9295@gmail.com>
2025-08-26 22:06:19 -0400

    Add \`TypeError\` unit test, update AI usage notice

--- test_utils.py
+++ test_utils.py
@@ -12,5 +12,8 @@ class TestIsPalindromeMethod(unittest.TestCase):
     def test_palindrome_with_special_characters(self):
         self.assertTrue(utils.is_palindrome('A man, a plan, a canal - Panama!'))
 
+    def test_non_string_failure(self):
+        self.assertRaises(TypeError, utils.is_palindrome, 1)
+
 if __name__ == '__main__':
     unittest.main()
